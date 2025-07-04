8a29f7be0bb1ecddfa372bee5b53d32faa0b3d31
Soywaz <soywaz@outlook.com>
2025-07-03 21:57:37 -0500

    modificación rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { HashRouter } from "react-router-dom";
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from "./components/Login/Login.jsx";
